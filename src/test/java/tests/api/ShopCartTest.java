9b73488c72e195b81cfdcf67c7ac32d2bf1b7e52
Roman <pcuxman@yandex.ru>
2021-09-04 14:28:48 +0300

    Тест на добавление товара в корзину и проверка наличия товара в корзине, добавление аллюр отчетности

--- src/test/java/tests/api/ShopCartTest.java
+++ src/test/java/tests/api/ShopCartTest.java
@@ -1,3 +1,12 @@
+package tests.api;
+
+import annotations.JiraIssue;
+import annotations.JiraIssues;
+import annotations.Layer;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Cookie;
 
@@ -5,17 +14,24 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 import static java.time.zone.ZoneRulesProvider.refresh;
 
 import static org.hamcrest.Matchers.is;
 
-
+@Layer("Api")
+@Owner("roman")
+@Feature("Issues")
 public class ShopCartTest {
 
     @Test
+    @JiraIssues({@JiraIssue("RK-01")})
+    @Tag("Api")
+    @DisplayName("Добавление товара в корзину")
     public void addProductInCartTest() {
         //Получить куки и установить в браузер
+        step("Получить куки и установить в браузер", () -> {
             String authorizationCookie =
                     given()
                             .contentType("application/x-www-form-urlencoded; charset=UTF-8")
@@ -32,21 +48,24 @@ public class ShopCartTest {
                             .extract()
                             .cookie("Nop.customer");
 
+            step("Открыть небольшой контент для активной сессии, куда можно подставить куки", () -> {
             //Открыть небольшой контент для активной сессии, куда можно подставить куки
                     open("http://demowebshop.tricentis.com/Themes/DefaultClean/Content/images/mobile-menu-collapse.png");
-
+            });
+                    step("Установить куки в браузер", () -> {
             //Установить куки в браузер
                     getWebDriver().manage().addCookie(
                             new Cookie("Nop.customer", authorizationCookie));
             refresh();
-
-
+                    });
+                    step("Открыть главную страницу", () -> {
         //Открыть главную страницу
             open("http://demowebshop.tricentis.com/");
-
+                    });
+                        step("Проверить актуальное количество товара в корзине", () -> {
             //Проверить актуальное количество товара в корзине
             $(".cart-qty").shouldHave(text("1"));
-
+                        });
+        });
     }
-
 }
